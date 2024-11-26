256590db9a329489b2daa417a208f0549d162e1c
JoceIT98 <evelyndeluna083@gmail.com>
2024-11-25 22:42:19 -0600

    Update codigo1.py

--- codigo/codigo.py
+++ codigo/codigo.py
@@ -1,6 +1,6 @@
 # Programa que solicita el nombre del usuario e imprime el un mensaje
 # personalizado con el nombre ingresado
 
-nombre=input("iIngresa tu nombre:  ")
+nombre=input("Ingresa tu nombre:  ")
 print("¡Hola! " + nombre + " como te encuentras el dia de hoy")
 
